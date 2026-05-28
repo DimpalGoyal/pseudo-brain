a57cd52a31f8222ccb376fe6bcc5a1c3eb9d6aa0
Dimpal Goyal <dimpalgoyal129@gmail.com>
2026-05-28 08:32:36 +0000

    added Card-component with link embeds

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,35 @@
-import { Button } from "./components/Button"
-import { ShareIcon } from "./icons/PlusIcon"
-import { PlusIcon } from "./icons/ShareIcon"
+import { Button } from "./components/Button";
+import { Card } from "./components/Card";
+import { ShareIcon } from "./icons/PlusIcon";
+import { PlusIcon } from "./icons/ShareIcon";
 
 function App() {
-
   return (
     <>
-    <div>
-    <Button variant="primary" text="add" startIcon={<PlusIcon/>}/>
-    <Button variant="secondary" text="share" startIcon={<ShareIcon/>}/>
-
-    </div>
+      <div className="m-15">
+        <div className="flex gap-2 justify-end">
+          <Button
+            variant="primary"
+            text="add content"
+            startIcon={<PlusIcon />}
+          />
+          <Button variant="secondary" text="share" startIcon={<ShareIcon />} />
+        </div>
+        <div className="pt-4 flex gap-4">
+          <Card
+            title="ai demo"
+            link="https://www.youtube.com/watch?v=wrH66MOWqGM"
+            type="youtube"
+          />
+          <Card
+            title="ai demo"
+            link="https://twitter.com/x/status/807811447862468608"
+            type="twitter"
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,51 @@
+import { ShareIcon } from "../icons/PlusIcon";
+
+interface cardProps {
+  title: string;
+  link: string;
+  type: "youtube" | "twitter";
+}
+
+export function Card({ title, link, type }: cardProps) {
+  return (
+    <div>
+      <div className="p-6 border  shadow-md max-w-72 rounded-md">
+        <div className="flex items-center justify-between ">
+          <div className="flex items-center">
+            <div className="pr-2">
+              <ShareIcon />
+            </div>
+            <span className="text-xl font-semibold">{title}</span>
+          </div>
+          <div className="flex gap-2">
+            <ShareIcon />
+            <ShareIcon />
+          </div>
+        </div>
+        <div className="pt-4 flex justify-center items-center">
+          {type === "youtube" && (
+            <iframe
+              className="w-full rounded-md"
+              src={link.replace("watch", "embed").replace("?v=", "/")}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+            ></iframe>
+          )}
+          {type === "twitter" && (
+            <div className="w-full rounded-md items-center justify-center">
+              <blockquote className="twitter-tweet">
+                <a href={link.replace("x.com", "twitter.com")}></a>
+              </blockquote>
+              <script
+                async
+                src="https://platform.twitter.com/widgets.js"
+              ></script>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
