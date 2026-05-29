f94d36ada6b505fb03e294b9be275f95d9ceef5b
Dimpal Goyal <dimpalgoyal129@gmail.com>
2026-05-29 15:26:23 +0000

    made dashboard share-button working

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -6,6 +6,8 @@ import { ShareIcon } from "../icons/PlusIcon";
 import { PlusIcon } from "../icons/ShareIcon";
 import { Sidebar } from "../components/sidebar";
 import { useContents } from "../hooks/useContents";
+import { BACKEND_URL } from "../config";
+import axios from "axios";
 
 export function Dashboard() {
   const [openModal, setOpenModal] = useState(false);
@@ -42,6 +44,17 @@ export function Dashboard() {
               variant="secondary"
               text="share"
               startIcon={<ShareIcon />}
+              onClick={async()=>{
+                const res = await axios.post(`${BACKEND_URL}/brain/share`,{
+                  share: true
+                }, {
+                  headers: {
+                    'Authorization': localStorage.getItem('token')
+                  }
+                })
+                const shareUrl = `localhost:5173/share/${res.data.hash}`
+                alert(shareUrl)
+              }}
             />
           </div>
           <div className="pt-4 gap-3 flex flex-wrap">
